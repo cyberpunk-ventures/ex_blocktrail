6b9cd0bbfd0c7c887885a2679adbb1166210e87f
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-05-31 13:44:02 +0300

    block_tx function, upgrade of url manipulation

--- lib/ex_blocktrail.ex
+++ lib/ex_blocktrail.ex
@@ -1,16 +1,33 @@
 defmodule ExBlocktrail do
   use HTTPoison.Base
 
-  def latest_block(token) do
-    url = String.downcase(token) <> "/block/latest"
+
+  def latest_block(_) do
+    url = "/btc/block/latest?"
     with {:ok, response} <- ExBlocktrail.get(url),
           latest_block_data = BlockData.new(response.body),
       do: {:ok, latest_block_data}
   end
 
+  def block_txs(block_hash, options) do
+    url = "/btc/block/#{block_hash}/transactions?" <> URI.encode_query(%{page: options.page})
+    with {:ok, response} <- ExBlocktrail.get(url),
+          paged_data = PagedResponse.new(response.body),
+      do: {:ok, paged_data}
+  end
+
+  def all_block_txs(block_hash) do
+
+  end
+
   def process_url(url) do
     key = Application.get_env(:ex_blocktrail, :api_key)
-    "https://api.blocktrail.com/v1/" <> url <> "?" <> URI.encode_query(%{api_key: key})
+    url = url
+    |> YURI.parse
+    |> YURI.put("api_key", key)
+    |> to_string
+
+    "https://api.blocktrail.com/v1" <> url
   end
 
   def process_response_body(body) do
